bae9b184c709f3151ab6607538cff85a5dd4ba15
senecat <loic.senecat@etu.u-pec.fr>
2021-06-04 09:26:29 +0200

    Api object

---
+++ src/com/geosis/api/response/ApiZoneSpeciesResponse.java
@@ -0,0 +1,9 @@
+package com.geosis.api.response;
+
+import com.geosis.api.object.ZoneSpecies;
+
+import java.util.ArrayList;
+
+public class ApiZoneSpeciesResponse extends ApiResponse{
+    private ArrayList<ZoneSpecies> data;
+}
