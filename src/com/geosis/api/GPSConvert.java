3824a1f48290d749ebb234695a50c5e62cbefe12
senecat <loic.senecat@etu.u-pec.fr>
2021-06-06 13:16:01 +0200

    Test

---
+++ src/com/geosis/api/GPSConvert.java
@@ -0,0 +1,25 @@
+package com.geosis.api;
+
+
+import com.ludovic.vimont.GeoHashHelper;
+import com.ludovic.vimont.Location;
+
+public class GPSConvert {
+
+
+    public GPSConvert(){
+
+    }
+
+    public static String coordGPStoGeoHash(double latitude, double longitude){
+        Location location = new Location("GeoHash", latitude, longitude);
+        return GeoHashHelper.getGeohash(location);
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(coordGPStoGeoHash(35, 40));
+
+    }
+
+}
